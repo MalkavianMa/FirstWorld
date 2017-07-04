4f64ef84464de4e4a3867a90ad52dd3a919126f7
MalkavianMa <zxcv01243@126.com>
2017-07-04 17:42:27 +0800

    Construction
    Construction

---
+++ construction/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class Person
+    {
+       public  string _name;
+        public int _age;
+        public Person(string name, int age)
+        {
+            this._name = name;//实际参数
+            this._age = age;
+            //string _name = name;
+            //int _age = age;
+        }
+        public Person() {
+
+            this._name = "colby";//形式参数
+            this._age = 20;
+         //   string _name = "colby";
+          //  int _age = 20;
+        }
+    }
+}
---
+++ construction/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();//使用默认构造函数
+            Person p2 = new Person("M", 22);//使用带两个参数的构造函数
+            Console.WriteLine(p1._name+"/0"+p1._age+"/n"+p2._name+"/0"+p2._age);
+            Console.ReadLine();
+        }
+    }
+}
